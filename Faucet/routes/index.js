e4c58e5a5a2160a97ac984b561f472a02756b7c5
Roangelo Lacamento <roan.lacamento@gmail.com>
2019-11-16 19:45:38 +0800

    Faucet: Update captcha for the faucet form

--- Faucet/routes/index.js
+++ Faucet/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var svgCaptcha = require('svg-captcha');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Faucet Application' });
+});
+
+
+/* GET home page. */
+// router.post('/', function(req, res, next) {
+//   res.render('index', { title: 'Faucet Application' });
+// });
+
+router.get('/captcha', function (req, res) {
+  var captcha = svgCaptcha.create();
+  req.session.captcha = captcha.text;
+  
+  res.type('svg');
+  res.status(200).send(captcha.data);
 });
 
 module.exports = router;
