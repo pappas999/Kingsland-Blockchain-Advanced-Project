1632fff600a86550d9e049071dc6f9c473f09fee
Roangelo Lacamento <roan.lacamento@gmail.com>
2019-11-16 21:33:34 +0800

    Faucet : Added form field validations

--- Faucet/app.js
+++ Faucet/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+const flash = require('express-flash')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,15 +18,32 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('faucetSecret'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const isDevMode = process.env.NODE_ENV === 'development';
+
+// 1st change.
+if (!isDevMode) {
+  app.set('trust proxy', 1);
+}
+
 app.use(session({
-  secret: 'faucet secret',
+  secret: 'faucetSecret',
+  name: 'faucetSession',
   resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    sameSite: true,
+    httpOnly: true,
+    secure: false,
+  }
+
+}));
+
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- Faucet/routes/index.js
+++ Faucet/routes/index.js
@@ -2,20 +2,79 @@ var express = require('express');
 var router = express.Router();
 var svgCaptcha = require('svg-captcha');
 
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Faucet Application' });
+  res.render('index', { 
+      title: 'Faucet Application',
+      data: {},
+      errors: {}
+  });
 });
 
 
-/* GET home page. */
-// router.post('/', function(req, res, next) {
-//   res.render('index', { title: 'Faucet Application' });
-// });
+router.post('/', function(req, res, next) {
+  var blockchainAddress = req.body.blockchainAddress;
+  var nodeUrl = req.body.nodeUrl;
+  var captchaInput = req.body.captchaInput;
+  var errs = {};
+
+  console.log(blockchainAddress);
+  console.log(nodeUrl);
+  console.log(captchaInput);
+  console.log(req.session.captcha);
+
+  if (!blockchainAddress || !nodeUrl || !captchaInput) {
+    
+    if(!blockchainAddress) {
+      errs.blockchainAddress = {msg: 'Blockchain Address is required'};
+    }
+    if(!nodeUrl) {
+      errs.nodeUrl = {msg: 'Node URL is required'};
+    }
+    if(!captchaInput) {
+      errs.captchaInput = {msg: 'Captcha is required'};
+    }
+
+    res.render('index', {
+      title: 'Faucet Application',
+      data: req.body,
+      errors: errs
+    })
+  }
+  // At this point we are sure that the fields has all the data
+  // we now validate the data;
+
+  // validate address
+  var re = /[0-9A-Fa-f]{6}/g;
+  if(!re.test(blockchainAddress) || blockchainAddress.length != 20) {
+      errs.blockchainAddress = {msg : "Blockchain Address is invalid!"};
+      res.render('index', {
+        title: 'Faucet Application',
+        data: req.body,
+        errors: errs
+      });
+  }
+
+  // validate captcha
+  if(captchaInput !== req.session.captcha) {
+    errs.captchaInput = {msg: 'Invalid captcha input'};
+    res.render('index', {
+      title: 'Faucet Application',
+      data: req.body,
+      errors: errs
+    });
+  }
+
+  req.flash('success', 'Got coins from the faucet!');
+  res.redirect('/');
+});
 
 router.get('/captcha', function (req, res) {
   var captcha = svgCaptcha.create();
   req.session.captcha = captcha.text;
+  req.session.save();
   
   res.type('svg');
   res.status(200).send(captcha.data);
